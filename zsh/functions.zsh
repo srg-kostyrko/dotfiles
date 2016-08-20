b20388d1d34be29bea1cffff4b02dbf042f9c878
Sergey Kostyrko <srg.kostyrko@gmail.com>
2016-08-20 16:40:09 +0300

    Add install mode to run script
    It installs different software
    Accepts different options to run only part of instalation

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -151,3 +151,18 @@ function up()
     fi
     cd "${cdir}"
 }
+
+# git commit browser. needs fzf
+log() {
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` \
+      --bind "ctrl-m:execute:
+                echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R'"
+}
+
+# direct it all to /dev/null
+function nullify() {
+  "$@" >/dev/null 2>&1
+}
